e58f17a304ec3f776978c2dc7d9b7facb2482310
Husseina Murugi Shezad <hussxinamurugi@gmail.com>
2026-01-13 05:39:56 +0300

    added solution codes to GetRequester.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
+# lib/GetRequester.py
+
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content   # ← MUST be bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
